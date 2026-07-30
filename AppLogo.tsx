65b3f1728b0e607ecd96d84776060744228fbc4f
gabrielafeitosa5632-commits <gabrielafeitosa5632@gmail.com>
2026-07-29 23:21:53 -0300

    Add files via upload

---
+++ AppLogo.tsx
@@ -0,0 +1,20 @@
+interface AppLogoProps {
+  size?: number;
+  className?: string;
+}
+
+export function AppLogo({ size = 48, className }: AppLogoProps) {
+  return (
+    <img
+      src="/logo-new.jpeg"
+      width={size}
+      height={size}
+      className={className}
+      alt="PhytoPathometric logo"
+      style={{ objectFit: 'contain' }}
+    />
+  );
+}
+
+export default AppLogo;
+
